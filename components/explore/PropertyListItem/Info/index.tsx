61c850a50de8588930bc10dc5f52da94e53aa6ea
Niranjan Ramadas <mail@niranjan.me>
2021-05-17 17:08:56 +0530

    Make prices a little easier to read

--- components/explore/PropertyListItem/Info/index.tsx
+++ components/explore/PropertyListItem/Info/index.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 import { MapPropertyModel } from '@/components/explore/MapPropertyModel';
 import { Caption } from '@/components/typography/Caption';
-import { H6 } from '@/components/typography/H6';
+import { H5 } from '@/components/typography/H5';
 import { Overline } from '@/components/typography/Overline';
 import { cleanAddress } from '@/lib/cleanAddress';
 import { toString } from '@/lib/number';
@@ -17,9 +17,28 @@ interface Props {
 }
 
 export function Info(props: Props) {
+  const priceStr = toString(props.property.price);
+  const [whole, fractional] = priceStr.split('.');
+  const priceParts = whole.split(',');
+
   return (
     <footer className={cx(props.className, styles.container)}>
-      <H6>₹{toString(props.property.price)}</H6>
+      <div className={styles.price}>
+        <H5>
+          <span className={styles.priceRupee}>₹</span>
+          {priceParts.map((p, i) => (
+            <React.Fragment key={i}>
+              {p}
+              {i !== priceParts.length - 1 && (
+                <span className={styles.priceSymbol}>,</span>
+              )}
+            </React.Fragment>
+          ))}
+        </H5>
+        {fractional && (
+          <Caption className={styles.priceSymbol}>.{fractional}</Caption>
+        )}
+      </div>
       <div className={styles.metadata}>
         <div className={styles.address}>
           <Caption>{cleanAddress(props.property.location.address)}</Caption>
